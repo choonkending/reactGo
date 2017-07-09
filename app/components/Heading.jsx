d596b95124b28d363587fbab9f71bfa714adbc65
Ken Ding <aenkil@gmail.com>
2017-07-09 23:13:02 +1000

    Create Heading styled-component

---
+++ app/components/Heading.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default styled.h1`
+    font-family: 'Montserrat', Helvetica, Arial, sans-serif;
+    font-size: 28px;
+    color: #333f48;
+    font-weight: 700;
+    line-height: 48px;
+`;
+
